dd72db5ea3dcbf37721ba646fbf149eec4a12e94
rahulshivuni <rahul.shivuni@gmail.com>
2018-11-18 12:12:24 -0600

    Add files via upload

---
+++ Flicker/Flicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicker
+//
+//  Created by Rahul Shivuni on 11/18/18.
+//  Copyright © 2018 Rahul Shivuni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
